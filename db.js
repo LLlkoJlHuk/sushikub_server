a4b403cfd4d78f1a68b0731c20d709ed8277c591
LLlkoJlHuk <overkot12@gmail.com>
2025-08-18 15:17:35 +0700

    Revert "Add new setting for MySQL"
    This reverts commit 6d8222f6c57ec2044fd87d33b4879179aca08c74.

--- db.js
+++ db.js
@@ -1,17 +1,12 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config({ path: process.env.NODE_ENV == 'production' ? '.env' : '.env.development' });
+const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
-  process.env.DB_NAME, // Название базы данных
-  process.env.DB_USER, // Пользователь
-  process.env.DB_PASSWORD, // Пароль
-  {
-    dialect: 'mysql', // Изменено с 'postgres' на 'mysql'
-    host: process.env.DB_HOST, // Хост
-    port: process.env.DB_PORT, // Порт
-    dialectOptions: {
-      connectTimeout: 30000, // Увеличенный таймаут для надёжности
-    },
-    logging: process.env.NODE_ENV === 'production' ? false : console.log, // Отключение логов в продакшене
-  }
+	process.env.DB_NAME,  // Название базы данных
+	process.env.DB_USER,  // Пользователь
+	process.env.DB_PASSWORD,  // Пароль
+	{
+		dialect: "postgres",
+		host: process.env.DB_HOST,  // Хост
+		port: process.env.DB_PORT,  // Порт
+	}
 );
